46821bb797263617d3cdce589f7c6860ba39edc5
Linus Castelino <linus.castelino@toysrus.com>
2018-03-08 21:24:06 +0530

    Adding Fruit ATG Module

---
+++ FruitModule/src/com/tru/Fruit.java
@@ -0,0 +1,17 @@
+package com.tru;
+
+public class Fruit {
+	
+	String fruitName;
+
+	public String getFruitName() {
+		return fruitName;
+	}
+
+	public void setFruitName(String fruitName) {
+		this.fruitName = fruitName;
+	}
+	
+	
+
+}
---
+++ FruitModule/src/com/tru/FruitService.java
@@ -0,0 +1,20 @@
+package com.tru;
+
+public class FruitService {
+	
+	Fruit fruit;
+
+	public void printFruit()
+	{
+		System.out.println("Fruit : "+getFruit().getFruitName());
+	}
+
+	public Fruit getFruit() {
+		return fruit;
+	}
+
+	public void setFruit(Fruit fruit) {
+		this.fruit = fruit;
+	}
+	
+}
